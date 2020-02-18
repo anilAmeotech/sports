a9b05fdf3a03786e60f9ab1c9e4e99c901ada754
anil <anil.ameotech@gmail.com>
2020-02-18 13:00:23 +0530

    change in signIn ts

--- src/modules/account/component/SignIn.ts
+++ src/modules/account/component/SignIn.ts
@@ -20,32 +20,26 @@ export default class SignIn extends Vue {
         this.loader = true;
         this.errorMessage = '';
         this.formSubmit = true;
-        let userCred = { "email": this.form.email, "password":this.form.password};
+        let userCred = { "email": this.form.email, "password": this.form.password };
         console.log(userCred);
         authService.SignIn(userCred).then((response: any) => {
             this.formSubmit = false;
             this.loader = false;
             const res = response.data;
             console.log(response);
-            if (response.status === 200) {
-                localStorage.setItem('user', JSON.stringify(res.data));
-                this.$store.commit('userLogged', true);
-                // let User = JSON.parse(localStorage.getItem('user'));
-                // this.$store.state.User = User.username;
-                this.$router.push('/league');
-            } else {
-                this.errorMessage = 'Invalid email or password.';
-                setTimeout(() => {
-                    this.errorMessage = "";
-                }, 3000);
-            }
-        // }, (err: any) => {    
-        //     this.loader = false;
-        //     this.errorMessage = 'Invalid email or password.';           
-        //     setTimeout(() => {
-        //         this.errorMessage = "";
-        //     }, 3000);
-        //     this.formSubmit = false;
+            localStorage.setItem('user', JSON.stringify(res.data));
+            this.$store.commit('userLogged', true);
+            // let User = JSON.parse(localStorage.getItem('user'));
+            // this.$store.state.User = User.username;
+            this.$router.push('/league');
+        }, (err: any) => {
+            this.loader = false;
+            console.log('***********error**********')
+            this.errorMessage = 'Invalid email or password.';
+            setTimeout(() => {
+                this.errorMessage = "";
+            }, 3000);
+            this.formSubmit = false;
         });
     }
 }

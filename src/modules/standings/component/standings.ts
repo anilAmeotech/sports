6f3da3e20f8e6b41f910ce2de17a8c453b895f8f
anil <anil.ameotech@gmail.com>
2020-02-18 16:11:14 +0530

    change in standings page

--- src/modules/standings/component/standings.ts
+++ src/modules/standings/component/standings.ts
@@ -6,4 +6,14 @@ export default class standings extends Vue {
   created() {
     this.$store.commit('userLogged', true)
   }
+  data() {
+    return {
+      items: [         
+        { "name": "NBA 2018", "id": 1 },
+        { "name": "NBA 2019", "id": 2 },
+        { "name": "NBA 2020", "id": 3 },
+        { "name": "NBA 2021", "id": 4 }                      
+      ]
+    }
+  }  
 }  
--- src/modules/standings/component/standings.vue
+++ src/modules/standings/component/standings.vue
@@ -5,7 +5,7 @@
       <div class="container">
         <div class="bracket forward">
           <div class="round">
-            <div class="card">
+            <div class="card" v-for="item in items" v-bind:key="item.id">
               <div class="match">
                 <div class="team">
                   <div class="row brackets">
@@ -22,61 +22,7 @@
                   <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
                 </div>
               </div>
-            </div>
-            <div class="card">
-              <div class="match">
-                <div class="team">
-                  <div class="row">
-                    <div class="cols-6">
-                      <img src="../../../assets/jazz.png" height="40" width="40" />
-                    </div>
-                    <div class="cols-6">
-                      <img src="../../../assets/rockets.png" height="40" width="40" />
-                    </div>
-                  </div>
-                </div>
-                <div class="team">
-                  <p>Pick a Team</p>
-                  <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="match">
-                <div class="team">
-                  <div class="row">
-                    <div class="cols-6">
-                      <img src="../../../assets/jazz.png" height="40" width="40" />
-                    </div>
-                    <div class="cols-6">
-                      <img src="../../../assets/rockets.png" height="40" width="40" />
-                    </div>
-                  </div>
-                </div>
-                <div class="team">
-                  <p>Pick a Team</p>
-                  <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="match">
-                <div class="team">
-                  <div class="row">
-                    <div class="cols-6">
-                      <img src="../../../assets/jazz.png" height="40" width="40" />
-                    </div>
-                    <div class="cols-6">
-                      <img src="../../../assets/rockets.png" height="40" width="40" />
-                    </div>
-                  </div>
-                </div>
-                <div class="team">
-                  <p>Pick a Team</p>
-                  <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
-                </div>
-              </div>
-            </div>
+            </div>           
           </div>
           <div class="round">
             <div class="card">
@@ -169,7 +115,7 @@
         </div>
         <div class="bracket reverse">
           <div class="round">
-            <div class="card">
+            <div class="card" v-for="item in items" v-bind:key="item.id">
               <div class="match">
                 <div class="team">
                   <div class="row">
@@ -186,61 +132,7 @@
                   <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
                 </div>
               </div>
-            </div>
-            <div class="card">
-              <div class="match">
-                <div class="team">
-                  <div class="row">
-                    <div class="cols-6">
-                      <img src="../../../assets/jazz.png" height="40" width="40" />
-                    </div>
-                    <div class="cols-6">
-                      <img src="../../../assets/rockets.png" height="40" width="40" />
-                    </div>
-                  </div>
-                </div>
-                <div class="team">
-                  <p>Pick a Team</p>
-                  <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="match">
-                <div class="team">
-                  <div class="row">
-                    <div class="cols-6">
-                      <img src="../../../assets/jazz.png" height="40" width="40" />
-                    </div>
-                    <div class="cols-6">
-                      <img src="../../../assets/rockets.png" height="40" width="40" />
-                    </div>
-                  </div>
-                </div>
-                <div class="team">
-                  <p>Pick a Team</p>
-                  <b-form-input id="range-1" type="range" min="0" max="5"></b-form-input>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="match">
-                <div class="team">
-                  <div class="row">
-                    <div class="cols-6">
-                      <img src="../../../assets/jazz.png" height="40" width="40" />
-                    </div>
-                    <div class="cols-6">
-                      <img src="../../../assets/rockets.png" height="40" width="40" />
-                    </div>
-                  </div>
-                </div>
-                <div class="team">
-                  <p>Pick a Team</p>
-                  <b-form-input id type="range" min="0" max="5"></b-form-input>
-                </div>
-              </div>
-            </div>
+            </div>           
           </div>
           <div class="round">
             <div class="card">
@@ -303,68 +195,17 @@
         </div>
       </div>
     </div>
-    <h3>Bet on special events</h3>
-    <b-row class="custom-standings">
+   <h3>Bet on special events</h3>
+    <b-row class="custom-standings" v-for="item in items" v-bind:key="item.id">
       <b-col cols="2">
         <b-icon-alert-circle></b-icon-alert-circle>
       </b-col>
       <b-col cols="4">
-        <p>NBA 2018 MVP</p>
+        <p>{{item.name}}</p>
       </b-col>
       <b-col cols="6">
         <select class="custom-select">
           <option selected>Stephan Curry</option>
-          <option value="1">Start</option>
-          <option value="2">End</option>
-          <option value="3">Prizes</option>
-        </select>
-      </b-col>
-    </b-row>
-
-    <b-row class="custom-standings">
-      <b-col cols="2">
-        <b-icon-alert-circle></b-icon-alert-circle>
-      </b-col>
-      <b-col cols="4">
-        <p>NBA 20018 MOST POINTS</p>
-      </b-col>
-      <b-col cols="6">
-        <select class="custom-select">
-          <option selected>Kevin Durant</option>
-          <option value="1">Start</option>
-          <option value="2">End</option>
-          <option value="3">Prizes</option>
-        </select>
-      </b-col>
-    </b-row>
-
-    <b-row class="custom-standings">
-      <b-col cols="2">
-        <b-icon-alert-circle></b-icon-alert-circle>
-      </b-col>
-      <b-col cols="4">
-        <p>NBA 20018 AGP</p>
-      </b-col>
-      <b-col cols="6">
-        <select class="custom-select">
-          <option selected>Stephan Curry</option>
-          <option value="1">Start</option>
-          <option value="2">End</option>
-          <option value="3">Prizes</option>
-        </select>
-      </b-col>
-    </b-row>
-
-    <b-row class="custom-standings">
-      <b-col cols="2" class="icons">
-        <b-icon-alert-circle></b-icon-alert-circle>
-      </b-col>
-      <b-col cols="4">
-        <p>NBA 2018 FT%</p>
-      </b-col>
-      <b-col cols="6">
-        <select class="custom-select">
-          <option selected>Kevin Durant</option>
           <option value="1">Start</option>
           <option value="2">End</option>
           <option value="3">Prizes</option>
